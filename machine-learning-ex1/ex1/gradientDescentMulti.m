75bf8bd103932f1f282a13cf0adbbc12de0310f1
Rahul Jain <rahul_jain@live.in>
2017-06-04 11:40:14 +0530

    cost/gradient multi

--- machine-learning-ex1/ex1/gradientDescentMulti.m
+++ machine-learning-ex1/ex1/gradientDescentMulti.m
@@ -11,14 +11,23 @@ for iter = 1:num_iters
 
     % ====================== YOUR CODE HERE ======================
     % Instructions: Perform a single gradient step on the parameter vector
-    %               theta. 
+    %               theta.
     %
     % Hint: While debugging, it can be useful to print out the values
     %       of the cost function (computeCostMulti) and gradient here.
     %
 
 
+    thetaNew = zeros(length(theta))(1);
+    for j=1:length(theta)
+        s = 0;
+        for i=1:m
+            s = s + ((theta' * X(i,:)') - y(i)) * X(i,j);
+        end
+        thetaNew(j) = theta(j) - alpha*(1/m)*s;
+    end
 
+    theta = thetaNew';
 
 
 
@@ -29,7 +38,7 @@ for iter = 1:num_iters
 
     % ============================================================
 
-    % Save the cost J in every iteration    
+    % Save the cost J in every iteration
     J_history(iter) = computeCostMulti(X, y, theta);
 
 end
